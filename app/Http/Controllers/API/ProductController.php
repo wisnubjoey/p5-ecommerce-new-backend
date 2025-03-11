06aa3a3d31e10efafa4856a1649dfd6b5f16b813
Wisnu Bjoey <Bjoey@Ekas-MacBook-Pro-2.local>
2025-03-12 01:27:25 +0800

    ProductController added product stock

--- app/Http/Controllers/API/ProductController.php
+++ app/Http/Controllers/API/ProductController.php
@@ -24,6 +24,7 @@ class ProductController extends Controller
             'description' => 'required|string',
             'main_photo_url' => 'required|string',
             'price' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
         ]);
 
         $product = Product::create($validated);
