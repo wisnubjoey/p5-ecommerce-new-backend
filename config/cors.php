2b345e5e13e42a7df9e6282262d71629db500e6f
Wisnu Bjoey <Bjoey@Ekas-MacBook-Pro-2.local>
2025-03-14 00:52:40 +0800

    ayo berhasil

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000', 'https://p5-ecommerce-new-frontend.vercel.app'],
+    'allowed_origins' => ['http://localhost:3000', 'https://p5-ecommerce-new-frontend.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
